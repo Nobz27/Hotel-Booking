614ed977dfc3726e88af5aa21a8d2ac288068263
Nobz27 <kayondonorbert@icloud.com>
2025-04-29 11:29:21 +0300

    added images, styles

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql"); // connecting my database
+
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hotel_lobo",
+});
 
 const app = express();
 
 //middleware
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); // path -- nested routs /booking/user/id
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //for parsing application/x-www-form-urlencoded - form data
 
 //routes
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  dbConnection.query("SELECT * FROM rooms", (roomsSelectError, rooms) => {
+    if (roomsSelectError) {
+      res.status(500).send("server Error:500");
+    } else {
+      dbConnection.query("SELECT * FROM spots", (spotsSelectError, spots) => {
+        if (spotsSelectError) {
+          res.status(500).send("Server Error: 500");
+        } else {
+          console.log(spots);
+          res.render("index.ejs", { rooms, spots });
+        }
+      });
+    }
+  });
 });
 // app.get("/", (req, res) => {
 //     res.send("Hello World");
