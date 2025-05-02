abfa7d87cb9693085d785442dd87dfd964fbe79d
Nobz27 <kayondonorbert@icloud.com>
2025-05-02 12:27:35 +0300

    authorisation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mysql = require("mysql"); // connecting my database
 const bcrypt = require("bcrypt");
 const session = require("express-session");
+const { log } = require("console");
 
 const dbConnection = mysql.createConnection({
   host: "localhost",
@@ -17,6 +18,45 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public"))); // path -- nested routs /booking/user/id
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //for parsing application/x-www-form-urlencoded - form data
+app.use(
+  session({
+    secret: "ujipower", //secret key for signing the session ID cookie
+    resave: false, // forces session to be saved back to the session store, even is the session was modified
+    saveUninitialized: true, // forces a session that is "Uninitialized" to be saved to the store
+  })
+);
+// authorization middleware
+const superAdminRoutes = ["/newSpot", "/newRoom", "/checkout"];
+const receptionistRoutes = ["/checkout", "/checkin", "/roomUpdates"];
+const managerRoutes = ["/addReceptionist", "/roomUpdates"];
+
+//all other routes are public
+app.use((req, res, next) => {
+  if (req.session.user) {
+    //user islogged in --- go head and check role and route they are accessing
+    const userRole = req.session.user.role; // get user role from session
+    if (userRole === "superadmin" && superAdminRoutes.includes(req.path)) {
+      next();
+    } else if (
+      userRole === "reception" &&
+      receptionistRoutes.includes(req.path)
+    ) {
+      next();
+    } else if (userRole === "manager" && managerRoutes.includes(req.path)) {
+      next();
+    }
+  } else {
+    //user is not logged in
+    if (
+      superAdminRoutes.includes(req.path) ||
+      receptionistRoutes.includes(req.path)
+    ) {
+      res.status(401).send("Unauthorized - 401");
+    } else {
+      next();
+    }
+  }
+});
 
 //routes
 
@@ -39,22 +79,58 @@ app.get("/", (req, res) => {
 // app.get("/", (req, res) => {
 //     res.send("Hello World");
 //   });
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/newSpot", (req, res) => {
+  res.render("newSpot.ejs");
+});
+
+app.get("/newRoom", (req, res) => {
+  res.render("newRoom.ejs");
+});
+
+app.get("/checkout", (req, res) => {
+  res.render("checkout.ejs");
+});
+
+app.get("/addReceptionist", (req, res) => {
+  res.render("addReceptionist.ejs");
+});
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
+  dbConnection.query(
+    `SELECT * FROM users WHERE email="${email}"`,
+    (error, userData) => {
+      if (error) {
+        res.status(500).send("server Error:500");
+      } else {
+        if (userData.length == 0) {
+          res.status(401).send("User not found");
+        } else {
+          // user found - check password
+          const user = userData[0];
+          // res.json(user); sends data to the browser which we don t want
+          const isPasswordValid = bcrypt.compareSync(password, user.password);
+          if (isPasswordValid) {
+            // password is valid - create session - express session middleware
+            req.session.user = user; // creating a session
+            res.send("login successful - valid password");
+          } else {
+            //password is invalid
+            res.status(401).send("Invalid password");
+          }
+        }
+      }
+    }
+  );
 });
-//  dbConnection.query('SELECT * FROM users WHERE email = ?', [email], async (err, results)
-//       if (err|| results.length === 0) return res.status(404).send('invalid Credentials')
+// console.log(bcrypt.hashSync("admin758", 3)); // hash password for testing
+// console.log(bcrypt.hashSync("admin245", 2));
+console.log(bcrypt.hashSync("admin123", 1));
 
-//         const user = results[0];
-//         const validPass = await bcrypt.compare(password, user.password)
-//         if(!validPass) return res.status(401).send('incorrect password')
-//           req.session.user = {
-//         id:user.id,
-//         email: user.email,
-//         role:user.role,
-//           }
-// )
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
